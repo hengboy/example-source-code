a31117f3bcd6f346163ec4af2db7ae5ef5004de3
恒宇少年 <39233436+hengboy@users.noreply.github.com>
2022-11-03 14:39:27 +0800

    :art: 调整获取授权用户对象的方式

--- github-oauth2-client/src/main/java/org/minbox/learning/github/oauth2/client/web/IndexController.java
+++ github-oauth2-client/src/main/java/org/minbox/learning/github/oauth2/client/web/IndexController.java
@@ -1,11 +1,9 @@
 package org.minbox.learning.github.oauth2.client.web;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 /**
  * @author 恒宇少年
@@ -13,11 +11,9 @@ import java.security.Principal;
 @RestController
 public class IndexController {
     @GetMapping
-    public String index(Principal principal) {
-        OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) principal;
-        OAuth2User user = authenticationToken.getPrincipal();
-        String name = user.getAttribute("name");
-        String account = user.getAttribute("login");
+    public String index(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        String name = oAuth2User.getAttribute("name");
+        String account = oAuth2User.getAttribute("login");
         return "GitHub OAuth2登录成功，欢迎：" + name + "(" + account + ")" + ",访问首页.";
     }
 }
